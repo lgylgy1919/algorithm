e86c699d1afcf7f9f96760b3aa26194ea0efe02f
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2021-02-09 00:20:14 +0900

    #8 Q14(2)

--- python_book/8 Linked List/Q14 Merge Two Sorted Lists.py
+++ python_book/8 Linked List/Q14 Merge Two Sorted Lists.py
@@ -5,6 +5,27 @@ l2 = [1, 3, 4]
 
 def merge_two_lists1(l1, l2):
     answer = []
+    for i in range(0, len(l1)+len(l2)):
+        if l1 and l2:
+            if l1[0] <= l2[0]:
+                answer.append(l1.pop(0))
+            else:
+                answer.append(l2.pop(0))
+        elif l1:
+            answer.append(l1.pop(0))
+        elif l2:
+            answer.append(l2.pop(0))
+    print(answer)
+    return answer
 
 
 merge_two_lists1(l1, l2)
+
+
+# solution
+def mergeTwoLists(self, l1, l2):
+    if(not l1) or (l2 and l1.val > l2.val):
+        l1, l2 = l2, l1
+    if l1:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+    return l1
