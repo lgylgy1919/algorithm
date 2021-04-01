56724030f450b4f85ef397a40ceeda6a0394a7d4
jaeyoon <lgylgy1919@gmail.com>
2021-04-01 23:56:36 +0900

    new

--- practice.py
+++
@@ -1,6 +0,0 @@
-import collections
-
-
-node = collections.defaultdict()
-for char in "apple":
-    node = node.child
---
+++ python_book/17 Sorting/Sort theory.py
@@ -0,0 +1,22 @@
+def bubblesort(A):
+    for i in rangie(1, len(A)):
+        for j in range(0, len(A - 1)):
+            if A[j] > A[j + 1]:
+                A[j], A[j + 1] = A[j + 1], A[j]
+
+
+def quicksort(A, lo, hi):
+    def partition(lo, hi):
+        pivot = A[hi]
+        left = lo
+        for right in range(lo, hi):
+            if A[right] < pivot:
+                A[left], A[right] = A[right], A[left]
+                left += 1
+        A[left], A[hi] = A[hi], A[left]
+        return left
+
+    if lo < hi:
+        pivot = partition(lo, hi)
+        qucksort(A, lo, pivot - 1)
+        quicksort(A, pivot + 1, hi)
